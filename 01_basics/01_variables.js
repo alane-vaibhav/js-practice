8206af6dff2d620cf20467b7aadde7c614902d68
alane-vaibhav <114981319+alane-vaibhav@users.noreply.github.com>
2025-01-31 19:07:01 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+var userName='Vaibhav';
+let userEmail='alane.vaibhav@gmail.com';
+const userId=12345;
+userCity='Pune';
+
+
+// userName='Alane'; not alllowed to reinitialize const variable:- assignment to const variable
+userEmail='vaibhav@gmail.com';
+userId=100001;
+userCity='Katraj'
+
+console.log('variable name');
+console.log(['variable names'])
+
+// prefer not to use var keyword
+// let and const introduced in ES6 features
+
+// we can assign memory by using just variable name as well, it should not must to initialized and it will show undefined value
