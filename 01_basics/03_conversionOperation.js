08acd3a26e43b445b71c1ba582a66bae9313849f
alane-vaibhav <114981319+alane-vaibhav@users.noreply.github.com>
2025-01-31 19:31:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score=33
+//console.log(typeof score);
+
+let scoreIs="22"
+let valueOfscore=Number(scoreIs)
+// console.log(typeOf valueOfscore)----> number
+
+let num="33abc"
+let newNum=Number(num)
+// console.log(typeof newNum)---> number
+// but
+
+// console.log(newNum)-----> NaN;
+
+let myscore=null
+//console.log(typeof myscore);
+
+let myCon=Number(myscore)
+// console.log(myscore);---- null
+
+// console.log(myCon)----> 0
+
+// for undefined---> type is NaN but value is undefined
+
+// for true/boolean--- 0/1
+
+//which is not able to convert will be NaN
+
+// type of NaN is ----- number
+
+
+// boolean of 0--- false
+// boolean of 1 is ---- true
+// boolean of string/blank string ---- true 
+// boolean of empty string---- false
+// boolean of empty array--- true
+// boolean of empty object--- true
+
+console.log(typeof undefined);
